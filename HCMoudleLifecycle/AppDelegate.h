c24f5390ab3d27918a0dff739146e0ba0ec8b70d
hechao <1054134542@qq.com>
2019-02-15 15:47:48 +0800

    Initial Commit

---
+++ HCMoudleLifecycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCMoudleLifecycle
+//
+//  Created by hechao on 2019/2/15.
+//  Copyright © 2019 hechao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
